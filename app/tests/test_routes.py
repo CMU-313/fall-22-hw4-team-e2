0a9a5c396db7869739e70a4b9a74270e1f22c976
Fiona Chiu <98993549+fionaaac@users.noreply.github.com>
2022-11-01 22:04:46 -0400

    added tests for predict route (invalid/valid inputs) (#4)
    resolved merge conflicts for additional tests sending client requests with local data from csv file

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -27,7 +27,6 @@ def test_local_data():
         assert response.status_code == 200
     assert response.status_code == 200
 
-    
 def test_base_route():
     app = Flask(__name__)
     configure_routes(app)
@@ -38,4 +37,23 @@ def test_base_route():
 
     assert response.status_code == 200
     assert response.get_data() == b'try the predict route it is great!'
- 
+
+def test_predict_route_valid_input():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/predict?age=18&absences=19&health=3'
+
+    response = client.get(url)
+
+    assert response.status_code == 200
+
+def test_predict_route_invalid_input():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/predict?age=18&absences=19'
+
+    response = client.get(url)
+
+    assert response.status_code == 500
